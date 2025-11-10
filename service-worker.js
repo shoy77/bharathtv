4317eb14782e22b2a907179f41c9f7cfd1cd535c
shoy77 <shoy77@gmail.com>
2025-11-10 22:19:37 +0530

    Refactor service worker to use CACHE_NAME constant

--- service-worker.js
+++ service-worker.js
@@ -1,19 +1,41 @@
+const CACHE_NAME = 'bharathtv-cache-v2';
+const urlsToCache = [
+  '/bharathtv/',
+  '/bharathtv/index.html',
+  '/bharathtv/manifest.json',
+  '/bharathtv/icons/icon-192x192.png',
+  '/bharathtv/icons/icon-512x512.png'
+];
+
+// Install event
 self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open('bharathtv-cache').then((cache) => {
-      return cache.addAll([
-        '/bharathtv/',
-        '/bharathtv/index.html',
-        '/bharathtv/manifest.json',
-        '/bharathtv/icons/icon-192x192.png',
-        '/bharathtv/icons/icon-512x512.png'
-      ]);
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
     })
   );
+  self.skipWaiting();
 });
 
+// Activate event (clear old caches)
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) =>
+      Promise.all(
+        cacheNames.map((cache) => {
+          if (cache !== CACHE_NAME) return caches.delete(cache);
+        })
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+// Fetch event
 self.addEventListener('fetch', (event) => {
   event.respondWith(
-    caches.match(event.request).then((response) => response || fetch(event.request))
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
   );
 });
