0ab1b3ba7a6bb46fcfc56ef3f9670f85142cd48c
shoy77 <shoy77@gmail.com>
2025-11-10 18:48:28 +0530

    Implement service worker for caching and fetch handling

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,14 @@
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open('bharathtv-cache').then((cache) => {
+      return cache.addAll(['./', './index.html', './manifest.json']);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => response || fetch(event.request))
+  );
+});
 
